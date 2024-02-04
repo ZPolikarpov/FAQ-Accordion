7629fde9e595f482f93920221da9facb4af70fe0
UltimateToaster <polikarpovzakhar@gmail.com>
2024-02-04 15:16:50 +0100

    Phone version

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import background from "./assets/images/background-pattern-desktop.svg"
 import star from "./assets/images/icon-star.svg"
 import Accordion from "./components/Accordion"
-import React, { useState } from "react"
 
 function App() {
 
 	return (
 		<>
-			<img src={background} className="background__image"/>
 			<main className="container">
 				<div className="card">
 					<div className="card__header">
